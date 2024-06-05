99aab56de1030f64183b17220b8d5c4a7accff6a
Stefano Buico <stefano.buico@gmail.com>
2024-06-05 16:45:07 +0200

    Added Nether coordinates (WIP)

--- src/main/java/net/hawkelele/simplecoordinatestools/ui/CoordinatesOverlay.java
+++ src/main/java/net/hawkelele/simplecoordinatestools/ui/CoordinatesOverlay.java
@@ -5,6 +5,7 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import org.apache.commons.lang3.StringUtils;
 
+
 public class CoordinatesOverlay implements HudRenderCallback {
     private final MinecraftClient client = MinecraftClient.getInstance();
     private boolean display = true;
@@ -54,27 +55,39 @@ public class CoordinatesOverlay implements HudRenderCallback {
                 // Cardinal/Ordinal facing direction
                 getCurrentFacingOrdinal(),
                 // Coordinates
-                Math.round(client.player.getX()), Math.round(client.player.getY()), Math.round(client.player.getZ()),
+                Math.round(client.player.getX()),
+                Math.round(client.player.getY()),
+                Math.round(client.player.getZ()),
                 // Clock
-                getCurrentClock());
+                getCurrentClock()
+        );
+
+        String otherDimensionContent = String.format("        (%s %s %s)        ",
+                Math.round(client.player.getX() * (client.world.getDimension().coordinateScale() == 1.0 ? 0.125 : 8.0)),
+                Math.round(client.player.getY() * (client.world.getDimension().coordinateScale() == 1.0 ? 0.125 : 8.0)),
+                Math.round(client.player.getZ() * (client.world.getDimension().coordinateScale() == 1.0 ? 0.125 : 8.0))
+        );
 
         // Initial vertical position for the overlay
-        int startingPosY = client.player.isInCreativeMode() ? 12 : 60;
+        int startingPosY = 65;
 
         // Number of HUD rows the text should be pushed up based on what elements are displayed
-        int modifier = (int) Math.floor(client.player.getHealth() / 10.0)
-                + (int) Math.floor(client.player.getAbsorptionAmount() / 10.0);
+        int modifier = client.player.isInCreativeMode() ? 0 :
+                (int) Math.floor(client.player.getHealth() / 10.0)
+                        + (int) Math.floor(client.player.getAbsorptionAmount() / 10.0);
+
 
         // Overlay placement
         int textPosX = Math.round(((float) client.getWindow().getScaledWidth() / 2) - ((float) client.textRenderer.getWidth(content) / 2));
+        int altTextPosX = Math.round(((float) client.getWindow().getScaledWidth() / 2) - ((float) client.textRenderer.getWidth("N   " + otherDimensionContent) / 2));
         int textPosY = client.getWindow().getScaledHeight() - (startingPosY + (modifier * 10));
         int linePosY = textPosY + (client.textRenderer.fontHeight);
 
         // Render the overlay
-        drawContext.drawText(client.textRenderer, (content), textPosX, linePosY, 0xEEEEEE, true);
+        drawContext.drawText(client.textRenderer, content, textPosX, linePosY, 0xEEEEEE, true);
+        drawContext.drawText(client.textRenderer, otherDimensionContent, altTextPosX, linePosY - 10, 0xAAAAAA, true);
 
     }
-
 
     /**
      * Hide the overlay
