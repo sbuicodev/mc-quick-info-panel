aa3c8e9c954bb6fc4c4ca156e5b14cfbb44ca52a
Stefano Buico <stefano.buico@gmail.com>
2024-06-05 11:18:06 +0200

    Initial commit

---
+++ src/main/java/net/hawkelele/simplecoordinatestools/SimpleCoordinatesTools.java
@@ -0,0 +1,40 @@
+package net.hawkelele.simplecoordinatestools;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.message.v1.ClientReceiveMessageEvents;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+
+import net.hawkelele.simplecoordinatestools.ui.CoordinatesOverlay;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+
+public class SimpleCoordinatesTools implements ClientModInitializer {
+    public static CoordinatesOverlay coordinatesOverlay = new CoordinatesOverlay();
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+    @Override
+    public void onInitializeClient() {
+        // On each in-game HUD render, the coordinates overlay gets rendered with it
+        HudRenderCallback.EVENT.register(coordinatesOverlay);
+
+        /*
+         * Every time a "Game message" (i.e.: a message that appears in the ActionBar) is received by the client
+         * the coordinates overlay should be hidden.
+         *
+         * Since there's no event handlers for the moment the message disappears, a time delay is applied instead.
+         */
+        ClientReceiveMessageEvents.ALLOW_GAME.register((message, overlay) -> {
+            coordinatesOverlay.hide();
+
+            // Apply a time delay before showing the coordinates overlay again
+            scheduler.schedule(() -> {
+                coordinatesOverlay.show();
+            }, 3, TimeUnit.SECONDS);
+            return overlay;
+        });
+    }
+
+}
---
+++ src/main/java/net/hawkelele/simplecoordinatestools/ui/CoordinatesOverlay.java
@@ -0,0 +1,92 @@
+package net.hawkelele.simplecoordinatestools.ui;
+
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import org.apache.commons.lang3.StringUtils;
+
+public class CoordinatesOverlay implements HudRenderCallback {
+    private final MinecraftClient client = MinecraftClient.getInstance();
+    private boolean display = true;
+
+    /**
+     * Translates in-game ticks to a human-readable 24h clock format
+     *
+     * @return The current time of the day in a 24h hh:mm format
+     */
+    protected String getCurrentClock() {
+        assert client.world != null;
+        long ticks = client.world.getTimeOfDay();
+        float hours = (((float) ticks / 1000) + 6) % 24;
+        float minutes = (hours * 60) % 60;
+
+        return StringUtils.leftPad(String.valueOf((int) Math.floor(hours)), 2, "0") + ":" + StringUtils.leftPad(String.valueOf((int) Math.floor(minutes)), 2, "0");
+
+    }
+
+    /**
+     * Shows the current facing direction as a single uppercase letter
+     *
+     * @return N, S, E, W according to the current player's facing cardinal direction
+     */
+    protected String getCurrentFacingOrdinal() {
+        assert client.player != null;
+        return StringUtils.capitalize(client.player.getHorizontalFacing().getName()).substring(0, 1);
+    }
+
+    protected void setDisplay(boolean value) {
+        display = value;
+    }
+
+    /**
+     * The actual overlay render implementation. This will get executed once per HUD render
+     *
+     * @param drawContext the {@link DrawContext} instance
+     * @param tickDelta   Progress for linearly interpolating between the previous and current game state
+     */
+    @Override
+    public void onHudRender(DrawContext drawContext, float tickDelta) {
+        if (client.options.hudHidden || client.player == null || client.world == null || !display) {
+            return;
+        }
+
+        String content = String.format("%s     %s %s %s     %s",
+                // Cardinal/Ordinal facing direction
+                getCurrentFacingOrdinal(),
+                // Coordinates
+                Math.round(client.player.getX()), Math.round(client.player.getY()), Math.round(client.player.getZ()),
+                // Clock
+                getCurrentClock());
+
+        // Initial vertical position for the overlay
+        int startingPosY = client.player.isInCreativeMode() ? 35 : 50;
+
+        // Number of HUD rows the text should be pushed up based on what elements are displayed
+        int modifier = (int) Math.floor(client.player.getHealth() / 10.0) + (int) Math.floor(client.player.getAbsorptionAmount() / 10.0) + (client.player.getArmor() > 0 ? 1 : 0);
+
+        // Overlay placement
+        int textPosX = Math.round(((float) client.getWindow().getScaledWidth() / 2) - ((float) client.textRenderer.getWidth(content) / 2));
+        int textPosY = client.getWindow().getScaledHeight() - (startingPosY + (modifier * 10));
+        int linePosY = textPosY + (client.textRenderer.fontHeight);
+
+        // Render the overlay
+        drawContext.drawText(client.textRenderer, (content), textPosX, linePosY, 0xEEEEEE, true);
+
+    }
+
+
+    /**
+     * Hide the overlay
+     */
+    public void hide() {
+        setDisplay(false);
+    }
+
+    /**
+     * Show the overlay
+     */
+    public void show() {
+        setDisplay(true);
+    }
+
+}
