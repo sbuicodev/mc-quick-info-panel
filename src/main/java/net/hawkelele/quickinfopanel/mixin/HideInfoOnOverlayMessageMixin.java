293350933e4bae7f3fb80dcd3b8c9ec5053ed59a
Stefano Buico <stefano.buico@gmail.com>
2024-06-16 21:22:38 +0200

    Changed inner workings of settings, fixed being able to change display settings of the alternate dimensions coordinates while panel was not visible

--- src/main/java/net/hawkelele/quickinfopanel/mixin/HideInfoOnOverlayMessageMixin.java
+++ src/main/java/net/hawkelele/quickinfopanel/mixin/HideInfoOnOverlayMessageMixin.java
@@ -4,6 +4,7 @@ import net.hawkelele.quickinfopanel.gui.panel.AlternateDimensionPanel;
 import net.hawkelele.quickinfopanel.gui.panel.Panel;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.render.RenderTickCounter;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,13 +13,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(InGameHud.class)
 public class HideInfoOnOverlayMessageMixin {
     @Inject(method = "renderOverlayMessage", at = @At("TAIL"))
-    public void hide(DrawContext context, float tickDelta, CallbackInfo ci) {
+    public void hide(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
         Panel.getInstance().hide();
         AlternateDimensionPanel.getInstance().hide();
     }
 
     @Inject(method = "renderOverlayMessage", at = @At("HEAD"))
-    public void show(DrawContext context, float tickDelta, CallbackInfo ci) {
+    public void show(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
         Panel.getInstance().show();
         AlternateDimensionPanel.getInstance().show();
     }
