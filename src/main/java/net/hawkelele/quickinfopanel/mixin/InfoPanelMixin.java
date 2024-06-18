bded49951998244ac014629430520a6241a4ea76
Stefano Buico <stefano.buico@gmail.com>
2024-06-18 17:23:16 +0200

    Backported icons from the 1.21 version

--- src/main/java/net/hawkelele/quickinfopanel/mixin/InfoPanelMixin.java
+++ src/main/java/net/hawkelele/quickinfopanel/mixin/InfoPanelMixin.java
@@ -5,15 +5,12 @@ import net.hawkelele.quickinfopanel.gui.panel.Panel;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.awt.*;
 
 @Mixin(InGameHud.class)
 public class InfoPanelMixin {
@@ -28,30 +25,14 @@ public class InfoPanelMixin {
             return;
         }
 
-        Text panelText = panel.getPanelText();
-        int[] textPosition = panel.getTextPosition(panelText);
-        context.drawText(
-                client.textRenderer,
-                panelText,
-                textPosition[0], textPosition[1],
-                Color.decode("#EEEEEE").hashCode(),
-                true
-        );
+        panel.draw(context);
 
         AlternateDimensionPanel alternateDimensionPanel = AlternateDimensionPanel.getInstance();
         if (alternateDimensionPanel.shouldBeHidden()) {
             return;
         }
 
-        Text alternateDimensionText = alternateDimensionPanel.getPanelText();
-        int[] altTextPosition = alternateDimensionPanel.getTextPosition(alternateDimensionText);
-        context.drawText(
-                client.textRenderer,
-                alternateDimensionText,
-                altTextPosition[0], altTextPosition[1],
-                Color.decode("#AAAAAA").hashCode(),
-                true
-        );
+        alternateDimensionPanel.draw(context);
     }
 
 }
