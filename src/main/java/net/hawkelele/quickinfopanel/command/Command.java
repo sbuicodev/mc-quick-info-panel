ffb2b929dd7026b56e7035dce7b2eca899794fa2
Stefano Buico <stefano.buico@gmail.com>
2025-12-11 00:28:28 +0100

    Updated to 1.21.11

--- src/main/java/net/hawkelele/quickinfopanel/command/Command.java
+++ src/main/java/net/hawkelele/quickinfopanel/command/Command.java
@@ -3,9 +3,10 @@ package net.hawkelele.quickinfopanel.command;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.Event;
 import net.hawkelele.quickinfopanel.config.Config;
+import org.jetbrains.annotations.NotNull;
 
 public abstract class Command {
-    protected final Event<ClientCommandRegistrationCallback> event = ClientCommandRegistrationCallback.EVENT;
+    protected final Event<@NotNull ClientCommandRegistrationCallback> event = ClientCommandRegistrationCallback.EVENT;
     protected final Config config = Config.getInstance();
 
 
