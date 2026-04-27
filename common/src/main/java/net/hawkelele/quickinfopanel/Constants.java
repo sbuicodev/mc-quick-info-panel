154cbebe21cdbcdf9ae0c20d855be3b6312a8ce9
Stefano Buico <stefano.buico@gmail.com>
2026-04-27 11:52:29 +0200

    chore(setup): change the project/mod ids and folders to reflect the actual package name

---
+++ common/src/main/java/net/hawkelele/quickinfopanel/Constants.java
@@ -0,0 +1,11 @@
+package net.hawkelele.quickinfopanel;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+    public static final String MOD_ID = "quickinfopanel";
+    public static final String MOD_NAME = "Quick Info Panel";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
